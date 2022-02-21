4d9c6e26c2fb2d3a6b3ecd3e443c09525a94383f
rominalodolo <lodoloromina@gmail.com>
2022-02-21 14:59:29 +0200

    update

---
+++ Classwork/src/main/java/Lesson5/Arrays.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson5;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        String[] names = {"Sammy", "Sally", "Sean"};
+        int [] ages = new int[3];
+        ages[0] = 35;
+        ages[1] = 20;
+        ages[2] = 27;    
+//      ages[3] = 34; out of bounds length exeption error 
+    }
+}
