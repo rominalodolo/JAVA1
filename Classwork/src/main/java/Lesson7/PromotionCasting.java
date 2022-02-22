820b02f1bcec2cf7f3a7deaa1e1cb1eb3e236e3b
rominalodolo <lodoloromina@gmail.com>
2022-02-22 14:26:15 +0200

    update

---
+++ Classwork/src/main/java/Lesson7/PromotionCasting.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson7;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class PromotionCasting {
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 2;
+//        double num2 = 2;
+        double num3;
+        num3 = num1 / num2;
+        
+        System.out.println("Result: " + num3);
+        
+        
+    }
+}
