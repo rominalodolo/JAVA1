71b5ca94a8d2fddd636ad69e42e54a041d4437fa
rominalodolo <lodoloromina@gmail.com>
2022-02-22 10:04:57 +0200

    update

--- Classwork/src/main/java/Lesson6/Employee.java
+++ Classwork/src/main/java/Lesson6/Employee.java
@@ -10,9 +10,58 @@ package Lesson6;
  * @author rominalodolo
  */
 public class Employee {
-    public String firstName;
-    public String lastName;
-    public int empId;
-    public int age;
-    public String homeAddress; 
+
+    private String firstName;
+    private String lastName;
+    private int empId;
+    private int age;
+    private String homeAddress;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public void empDetails(){
+        System.out.println("Name: " + firstName);
+        System.out.println("Surname: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Home Address: " + homeAddress);
+    }
 }
--- Classwork/src/main/java/Lesson6/Main.java
+++ Classwork/src/main/java/Lesson6/Main.java
@@ -12,16 +12,35 @@ package Lesson6;
 public class Main {
     public static void main(String[] args) {
         Employee emp1 = new Employee();
-        emp1.firstName = "Amanda";
-        emp1.lastName = "Ally";
-        emp1.empId = 001;
-        emp1.age = 43;
-        emp1.homeAddress = "50 Signal Rd, Riverside, Pretoria.";
-         
-        System.out.println("Name: " + emp1.firstName);
-        System.out.println("Surname: " + emp1.lastName);
-        System.out.println("Age: " + emp1.age);
-        System.out.println("Employee ID: " + emp1.empId);
-        System.out.println("Home Address: " + emp1.homeAddress);
+        Employee emp2 = new Employee();
+//        emp1.firstName = "Amanda";
+//        emp1.lastName = "Ally";
+//        emp1.empId = 001;
+//        emp1.age = 43;
+//        emp1.homeAddress = "50 Signal Rd, Riverside, Pretoria.";
+//         
+      
+        emp1.setAge(43);
+        emp1.setEmpId(001);
+        emp1.setFirstName("Amanda");
+        emp1.setLastName("Ally");
+        emp1.setHomeAddress("50 Signal Rd, Riverside, Pretoria."); 
+        
+        emp2.setAge(39);
+        emp2.setEmpId(002);
+        emp2.setFirstName("Anne");
+        emp2.setLastName("Gurdle");
+        emp2.setHomeAddress("2 Shepstone Gardens, SunnyHill, Pretoria."); 
+        
+        
+//        System.out.println("Name: " + emp1.firstName);
+//        System.out.println("Surname: " + emp1.lastName);
+//        System.out.println("Age: " + emp1.age);
+//        System.out.println("Employee ID: " + emp1.empId);
+//        System.out.println("Home Address: " + emp1.homeAddress);
+
+        emp1.empDetails();
+        emp2.empDetails();
+        
     }
 }
