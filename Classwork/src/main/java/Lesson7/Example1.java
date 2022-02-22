9ed1e2b84a1f7783c1147c7d35cd066a05dd7b33
rominalodolo <lodoloromina@gmail.com>
2022-02-22 13:44:43 +0200

    update

--- Classwork/src/main/java/Lesson7/Example1.java
+++ Classwork/src/main/java/Lesson7/Example1.java
@@ -18,5 +18,15 @@ public class Example1 {
 
         System.out.println(result1);
         System.out.println(result2);
+        
+        System.out.println("\n");
+        
+        String phoneNum = "404-567-9107";
+        int idx1 = phoneNum.indexOf('-');
+        System.out.println("index of first dash: " + idx1);
+        
+        int idx2 = phoneNum.indexOf('-', idx1+1);
+        System.out.println("second dash idx: "+idx2);
+        
     }
 }
