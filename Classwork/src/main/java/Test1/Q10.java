a3934517cd79162c284c98c1b523a8e311a614eb
rominalodolo <lodoloromina@gmail.com>
2022-02-23 11:49:34 +0200

    update

--- Classwork/src/main/java/Test1/Q10.java
+++ Classwork/src/main/java/Test1/Q10.java
@@ -13,11 +13,11 @@ public class Q10 {
     public static void main(String[] args) {
         boolean b1 = false;
         boolean b2 = false;
-        if (b2 != b1 = !b2){
-            System.out.println("true");
-        }
-        else{
-            System.out.println("false");
-        }
+//        if (b2 != b1 = !b2){
+//            System.out.println("true");
+//        }
+//        else{
+//            System.out.println("false");
+//        }
     }
 }
