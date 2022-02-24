bcdb7e40a24875b035df01ed4954c1084e3fd55f
rominalodolo <lodoloromina@gmail.com>
2022-02-24 15:14:21 +0200

    update

--- Classwork/src/main/java/Lesson9/ShoppingCart.java
+++ Classwork/src/main/java/Lesson9/ShoppingCart.java
@@ -12,8 +12,12 @@ package Lesson9;
 public class ShoppingCart {
     public static void main(String[] args) {
         Shirt shirt01 = new Shirt(12, "Floral", 'B', 12.87);
+        Shirt shirt02 = new Shirt(8, "Checkered", 'R');
+        Shirt shirt03 = new Shirt("Animal", 'Y', 13.18);
         shirt01.setColorCode('V');
         
-        System.out.println(shirt01.print());        
+        System.out.println(shirt01.print());
+        System.out.println(shirt02.print());
+        System.out.println(shirt03.print());
     }
 }
