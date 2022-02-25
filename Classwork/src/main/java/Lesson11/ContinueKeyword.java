d76e87c67adb07d793c47f0f95df1771398add51
rominalodolo <lodoloromina@gmail.com>
2022-02-25 14:30:48 +0200

    new

---
+++ Classwork/src/main/java/Lesson11/ContinueKeyword.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson11;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class ContinueKeyword {
+    public static void main(String[] args) {
+        for(int i = 0; i < 50; i++){
+            if((1 % 2) == 0){
+                continue;
+            }
+            System.out.println("i: " + i);
+        }
+    }
+}
