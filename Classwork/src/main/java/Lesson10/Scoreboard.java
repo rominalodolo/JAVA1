eb52239e558660416107b7d670e3fb24ed8e6d31
rominalodolo <lodoloromina@gmail.com>
2022-02-25 09:52:57 +0200

    new

---
+++ Classwork/src/main/java/Lesson10/Scoreboard.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson10;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class Scoreboard {
+
+    public static void main(String[] args) {
+        int numberOfGoals = 2;
+
+        System.out.println("I scored " + numberOfGoals + (numberOfGoals == 1 ? "goal" : "goals"));
+    }
+}
