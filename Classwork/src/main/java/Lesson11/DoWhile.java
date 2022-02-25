b78278a7172a80741ce6ca6d26dad12269c6fdba
rominalodolo <lodoloromina@gmail.com>
2022-02-25 14:27:05 +0200

    update

--- Classwork/src/main/java/Lesson11/DoWhile.java
+++ Classwork/src/main/java/Lesson11/DoWhile.java
@@ -10,5 +10,17 @@ package Lesson11;
  * @author rominalodolo
  */
 public class DoWhile {
-    
+
+    public static void main(String[] args) {
+        int i = 0;
+//    
+//        while (i < 3){
+//            System.out.println("i: " + i);
+//            i++;
+//        }
+        do {
+            System.out.println("i: " + i);
+            i++;
+        } while (i < 10);
+    }
 }
