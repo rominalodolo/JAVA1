e2f721b7cb04b6497aab1a039e21ba790c4ba40f
rominalodolo <lodoloromina@gmail.com>
2022-02-28 11:21:24 +0200

    new

--- Classwork/src/main/java/Lesson12/Shop/Dress.java
+++ Classwork/src/main/java/Lesson12/Shop/Dress.java
@@ -9,6 +9,37 @@ package Lesson12.Shop;
  *
  * @author rominalodolo
  */
-public class Dress {
-    
+public class Dress extends Clothing {
+
+    private String length;
+    private String dressDetails;
+
+    public Dress(String length, String dressDetails, String item, String description, int SKU, String materials, double price, double salePrice, char size) {
+        super(item, description, SKU, materials, price, salePrice, size);
+        this.length = length;
+        this.dressDetails = dressDetails;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getDressDetails() {
+        return dressDetails;
+    }
+
+    public void setDressDetails(String dressDetails) {
+        this.dressDetails = dressDetails;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("Length: " + getLength());
+        System.out.println("Dress Details: " + getDressDetails());
+    }
 }
--- Classwork/src/main/java/Lesson12/Shop/Skirts.java
+++ Classwork/src/main/java/Lesson12/Shop/Skirts.java
@@ -9,6 +9,37 @@ package Lesson12.Shop;
  *
  * @author rominalodolo
  */
-public class Skirts {
-    
+public class Skirts extends Clothing {
+
+    private String style;
+    private int waistSize;
+
+    public Skirts(String style, int waistSize, String item, String description, int SKU, String materials, double price, double salePrice, char size) {
+        super(item, description, SKU, materials, price, salePrice, size);
+        this.style = style;
+        this.waistSize = waistSize;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public int getWaistSize() {
+        return waistSize;
+    }
+
+    public void setWaistSize(int waistSize) {
+        this.waistSize = waistSize;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("Style: " + getStyle());
+        System.out.println("Waist Size: " + getWaistSize());
+    }
 }
