3f588f2eb23dac2cc993512c148b6f033aa7dd99
rominalodolo <lodoloromina@gmail.com>
2022-02-28 14:08:20 +0200

    update

--- Classwork/src/main/java/Exercise04_1/ShoppingCart.java
+++ Classwork/src/main/java/Exercise04_1/ShoppingCart.java
@@ -10,12 +10,19 @@ package Exercise04_1;
  * @author rominalodolo
  */
 public class ShoppingCart {
+
     public static void main(String[] args) {
 //        Declare and initialize 2 String variables: custName and itemDesc
         String custName = "Mary Smith";
         String itemDesc = "Shirt";
         String message;
-        
-       
+
+//        Assign the message variable with a concatenation of the custName and itemDesc.  
+//     Include a String literal that results in a complete sentence.  
+//     (example: "Mary Smith wants to purchase a Shirt")
+        message = custName + " wants to purchase a " + itemDesc;
+
+//        Print the message to the System output.
+        System.out.println(message);
     }
 }
