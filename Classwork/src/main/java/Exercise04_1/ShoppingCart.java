4229cc032d62068e7f2b28f853e2e51f96ade78e
rominalodolo <lodoloromina@gmail.com>
2022-02-28 14:00:50 +0200

    new

---
+++ Classwork/src/main/java/Exercise04_1/ShoppingCart.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercise04_1;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class ShoppingCart {
+    public static void main(String[] args) {
+//        Declare and initialize 2 String variables: custName and itemDesc
+        String custName = "Mary Smith";
+        String itemDesc = "Shirt";
+        String message;
+        
+       
+    }
+}
