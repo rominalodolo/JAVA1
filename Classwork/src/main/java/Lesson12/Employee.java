fa1ce1c621c7ff76a39bb9efcffc74bf31cc6dc0
rominalodolo <lodoloromina@gmail.com>
2022-02-28 09:57:58 +0200

    ud

--- Classwork/src/main/java/Lesson12/Employee.java
+++ Classwork/src/main/java/Lesson12/Employee.java
@@ -10,5 +10,78 @@ package Lesson12;
  * @author rominalodolo
  */
 public class Employee {
-    
+        private String firstName;
+        private String lastName;
+        private int empID;
+        private String address;
+        private String title;
+        private Double annualIncome;
+
+    public Employee(String firstName, String lastName, int empID, String address, String title, Double annualIncome) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.empID = empID;
+        this.address = address;
+        this.title = title;
+        this.annualIncome = annualIncome;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public void setAnnualIncome(Double annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+        
+    public void print(){
+        System.out.println("Name: " +  getFirstName());
+        System.out.println("Last Name: " +  getLastName());
+        System.out.println("Employee ID: " +  getEmpID());
+        System.out.println("Address: " +  getAddress());
+        System.out.println("Title: " +  getTitle());
+        System.out.println("Annual Income: " +  getFirstName());
+    }
+            
 }
+
---
+++ Classwork/src/main/java/Lesson12/Engineering.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson12;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class Engineering extends Employee {
+    private boolean fullstack;
+    private int yearsOfExperience;
+
+    public Engineering(boolean fullstack, int yearsOfExperience, String firstName, String lastName, int empID, String address, String title, Double annualIncome) {
+        super(firstName, lastName, empID, address, title, annualIncome);
+        this.fullstack = fullstack;
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public boolean isFullstack() {
+        return fullstack;
+    }
+
+    public void setFullstack(boolean fullstack) {
+        this.fullstack = fullstack;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+}
+
