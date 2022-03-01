c61d4a3d257e4a7f5e989640a445e25bcb4b3880
rominalodolo <lodoloromina@gmail.com>
2022-03-01 11:35:58 +0200

    ud

--- Classwork/src/main/java/Lesson14/MultipleExceptions/MultipleExceptions.java
+++ Classwork/src/main/java/Lesson14/MultipleExceptions/MultipleExceptions.java
@@ -10,5 +10,24 @@ package Lesson14.MultipleExceptions;
  * @author rominalodolo
  */
 public class MultipleExceptions {
-    
+    public static void main(String[] args) {
+        try{
+            createFile();
+        } catch (IOException ioe){
+            System.out.println(ioe);
+        } catch (IllegalArgumentException iae){
+            System.out.println(iae);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+        
+    public void createFile() throws Exception{
+        File testF = new File("textFile.txt");
+        File tempF = testF.createTempFile("te", null, testF);
+        System.out.println("Temp filename: " + tempF.getPath());
+        int myInt[] = new int[5];
+        myInt[5] = 25
+       
+    }
 }
