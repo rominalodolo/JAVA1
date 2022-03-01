5106c8168d9ec6bde926fd8af5c76ef407b750be
rominalodolo <lodoloromina@gmail.com>
2022-03-01 11:23:55 +0200

    update

---
+++ Classwork/src/main/java/Lesson14/TestClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson14;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class TestClass {
+    public void testCheckedException() throws Exception{
+        File testFile = new File("testFile.txt");
+        testFile.createNewFile();
+            System.out.println("testFile exists: " + testFile.exists());
+    }
+}
