407e35c044724a2eb888bfcf8b1768ab9c67f3c1
rominalodolo <lodoloromina@gmail.com>
2022-03-02 11:48:23 +0200

    new

---
+++ JavaDummies/src/main/java/BankAppExercise/UserAccount.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankAppExercise;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class UserAccount {
+    
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+        int start = 1000;
+        int withdrawl = UserWithdrawlAmount;
+        int add = UserAddAmount;
+        int totalFunds = ( start - withdrawl ) | ( start + add );
+
+        System.out.println("You currently have " + start + " in your account.");
+        if 
+        
+        System.out.println("Do you wish to add funds? Y/n");
+        
+        
+        System.out.println("Do you wish to withdraw funds? Y/n");
+        System.out.println("Thank you for banking with us, Have a lovely day!");
+        
+        
+        System.out.println("Total in " + UserAccount + ": " + totalFunds );
+    }
+}
