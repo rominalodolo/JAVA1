12d6c623f8043322e285ad9ab193d4fa6d8f1444
rominalodolo <lodoloromina@gmail.com>
2022-03-02 08:34:12 +0200

    update

---
+++ ClassQuiz2/src/main/java/Question1/objective1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Question1;
+
+/**
+ *
+ * @author rominalodolo
+ */
+public class objective1 {
+    
+    public class StaticImports{
+        
+        public StaticImports(){
+            out.println(MAX_VALUE);
+        }
+    }
+}
