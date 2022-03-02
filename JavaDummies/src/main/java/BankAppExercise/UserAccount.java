4826a5f739e2bb5b800413515546ac15cf32dd28
rominalodolo <lodoloromina@gmail.com>
2022-03-02 13:29:11 +0200

    update

--- JavaDummies/src/main/java/BankAppExercise/UserAccount.java
+++ JavaDummies/src/main/java/BankAppExercise/UserAccount.java
@@ -5,6 +5,7 @@
  */
 package BankAppExercise;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 /**
@@ -14,21 +15,27 @@ import java.util.Scanner;
 public class UserAccount {
     
     static Scanner sc = new Scanner(System.in);
+    static NumberFormat cf = NumberFormat.getCurrencyInstance();
     public static void main(String[] args) {
         
+        String UserInput = ("Y") ("N");
         int start = 1000;
         int withdrawl = UserWithdrawlAmount;
         int add = UserAddAmount;
         int totalFunds = ( start - withdrawl ) | ( start + add );
 
         System.out.println("You currently have " + start + " in your account.");
-        if 
         
+        
+        if (add.equalsIgnoreCase("y")) { 
         System.out.println("Do you wish to add funds? Y/n");
+        String add = sc.next();
         
         
         System.out.println("How much would you like to deposit?");
-
+        else {
+                
+                }
         
         System.out.println("Do you wish to withdraw funds? Y/n");
         
@@ -39,6 +46,6 @@ public class UserAccount {
         System.out.println("Thank you for banking with us, Have a lovely day!");
         
         
-        System.out.println("Total in " + UserAccount + ": " + totalFunds );
+        System.out.println("Total in " + UserAccount + ": " + cf.format(totalFunds) );
     }
 }
