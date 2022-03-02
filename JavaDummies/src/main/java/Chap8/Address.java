d301c8547018ffe0c8fa1ed97b5ea208ae8ef2bd
rominalodolo <lodoloromina@gmail.com>
2022-03-02 11:26:16 +0200

    new

---
+++ JavaDummies/src/main/java/Chap8/Address.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chap8;
+
+/**
+ *
+ * @author rominalodolo
+ */
+
+public class Address implements Cloneable
+{
+	public String street;
+	public String city;
+	public String state;
+	public String zipCode;
+}
