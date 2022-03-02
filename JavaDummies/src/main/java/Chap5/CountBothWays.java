79a6b73e03bd01d24f4cc148a0d94752b0773dc8
rominalodolo <lodoloromina@gmail.com>
2022-03-02 14:02:11 +0200

    update

--- JavaDummies/src/main/java/Chap5/CountBothWays.java
+++ JavaDummies/src/main/java/Chap5/CountBothWays.java
@@ -9,9 +9,6 @@ package Chap5;
  *
  * @author rominalodolo
  */
-public class TerseCoder {
-
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; System.out.println(i++));
-    }
+public class CountBothWays {
+    
 }
