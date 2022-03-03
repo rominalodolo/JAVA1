c52cf67153c28282f5bdf241a1ae7bb5cc4eb541
rominalodolo <lodoloromina@gmail.com>
2022-03-03 13:00:19 +0200

    up

--- JavaDummies/src/main/java/Book5Chap1/CountDownApp.java
+++ JavaDummies/src/main/java/Book5Chap1/CountDownApp.java
@@ -10,9 +10,17 @@ package Book5Chap1;
  * @author rominalodolo
  */
 public class CountDownApp {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Thread clock = new CountDownClock();
+//        clock.start();
+        Runnable flood, ignition, liftoff;
+        flood = new LaunchEvent(16, "Flood the pad!");
+        ignition = new LaunchEvent(6, "Start engines!");
+        liftoff = new LaunchEvent(0, "Liftoff!");
         clock.start();
+        new Thread(flood).start();
+        new Thread(ignition).start();
+        new Thread(liftoff).start();
     }
 }
