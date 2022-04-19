c3e25fceeb1678de8cb7eb249676be90b705a94e
rominalodolo <lodoloromina@gmail.com>
2022-04-19 18:56:31 +0200

    u

--- ClassQuiz3/src/my/school/Classroom.java
+++ ClassQuiz3/src/my/school/Classroom.java
@@ -1,13 +1,13 @@
-package my.school;
-
-public class Classroom {
-    private int roomNumber;
-    protected String teacherName;
-    static int globalKey = 54321;
-    public int floor = 3;
-    Classroom(int r, Sting t){
-        roomNumber = r;
-        teacherName = t;
-    }
-}
-
+//package my.school;
+//
+//public class Classroom {
+//    private int roomNumber;
+//    protected String teacherName;
+//    static int globalKey = 54321;
+//    public int floor = 3;
+//    Classroom(int r, Sting t){
+//        roomNumber = r;
+//        teacherName = t;
+//    }
+//}
+//
