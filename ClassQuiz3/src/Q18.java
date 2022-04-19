a68c14def980f1f2cf09ddffba3a74e5f95dc2a4
rominalodolo <lodoloromina@gmail.com>
2022-04-19 14:25:52 +0200

    update

--- ClassQuiz3/src/Q18.java
+++ ClassQuiz3/src/Q18.java
@@ -1,2 +1,16 @@
-public class Q18 {
-}
+//import java.util.ArrayList;
+//
+//public class Q18 {
+//    public static void main(String[] args) {
+//        List<String> one = new ArrayList<String>();
+//        one.add("abc");
+//        List<String> two = new ArrayList<>();
+//        two.add("abc");
+//        if (one == two)
+//            System.out.println("A");
+//        else if (one.equals(two))
+//            System.out.println("B");
+//        else
+//            System.out.println("C");
+//    }
+//}
