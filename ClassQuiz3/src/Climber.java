80d70db32fe2a8143ecd58941eb41b9b516c3cd6
rominalodolo <lodoloromina@gmail.com>
2022-04-19 18:56:21 +0200

    u

---
+++ ClassQuiz3/src/Climber.java
@@ -0,0 +1,14 @@
+// Question 24 cont.
+
+//public class Climber {
+//    public static void main(String[] args) {
+//        check((h, l) -> h.append(l).isEmpty(), 5);
+//    }
+//    private static void check(Climb climb, int height){
+//            if (climb.isTooHigh(height, 10))
+//                System.out.println("too high");
+//            else
+//                System.out.println("ok");
+//
+//    }
+//}
