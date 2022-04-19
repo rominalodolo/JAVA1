0a87817d7b933e310ac6f52427d9e87b1c03cdb5
rominalodolo <lodoloromina@gmail.com>
2022-04-19 14:14:30 +0200

    update

--- ClassQuiz3/src/Q17.java
+++ ClassQuiz3/src/Q17.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Q17 {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+        list.add(7);
+        for(String s : list) System.out.print(s);
+    }
 }
