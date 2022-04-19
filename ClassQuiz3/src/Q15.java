48afe052941994d4e31b2538daab944010ad3965
rominalodolo <lodoloromina@gmail.com>
2022-04-19 14:09:29 +0200

    update

--- ClassQuiz3/src/Q15.java
+++ ClassQuiz3/src/Q15.java
@@ -1,2 +1,11 @@
 public class Q15 {
+    public static void main(String[] args) {
+
+        int total = 0;
+        StringBuilder letters = new StringBuilder("abcdefg");
+        total += letters.substring(1,2).length();
+        total += letters.substring(6,6).length();
+        total += letters.substring(6,5).length();
+        System.out.println(total);
+    }
 }
