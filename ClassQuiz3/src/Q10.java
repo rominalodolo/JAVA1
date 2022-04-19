12042dc94f7c7d80cfce5b8f323290fa8ca6da50
rominalodolo <lodoloromina@gmail.com>
2022-04-19 13:56:59 +0200

    up

--- ClassQuiz3/src/Q10.java
+++ ClassQuiz3/src/Q10.java
@@ -1,2 +1,11 @@
 public class Q10 {
+    public static void main(String[] args) {
+        int m = 9, n = 1, x = 0;
+        while(m > n) {
+            m--;
+            n += 2;
+            x += m + n;
+        }
+        System.out.println(x);
+    }
 }
