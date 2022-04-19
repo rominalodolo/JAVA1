6308905c5872040a6fd7ffb9d15ac60f9441de3b
rominalodolo <lodoloromina@gmail.com>
2022-04-19 14:05:42 +0200

    update

--- ClassQuiz3/src/Q13.java
+++ ClassQuiz3/src/Q13.java
@@ -1,2 +1,8 @@
 public class Q13 {
+    public static void main(String[] args) {
+        String letters = "abcdef";
+        System.out.println(letters.length());
+        System.out.println(letters.charAt(3));
+        System.out.println(letters.charAt(6));
+    }
 }
