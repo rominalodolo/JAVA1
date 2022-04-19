cc07a90c344e434681ce339eee375207b830ce57
rominalodolo <lodoloromina@gmail.com>
2022-04-19 18:56:50 +0200

    u

---
+++ ClassQuiz3/src/Rope.java
@@ -0,0 +1,20 @@
+// Question 20
+
+public class Rope {
+    public static void swing(){
+        System.out.println("swing ");
+    }
+    public void climb(){
+        System.out.println("climb ");
+    }
+    public static void play(){
+        swing();
+//        climb();
+    }
+    public static void main(String[] args) {
+        Rope rope = new Rope();
+        rope.play();
+        Rope rope2 = null;
+        rope2.play();
+    }
+}
