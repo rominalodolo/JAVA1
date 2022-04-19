10309596f81fd2c92d8b7e4b554b4bca7bd20e89
rominalodolo <lodoloromina@gmail.com>
2022-04-19 13:06:23 +0200

    update

--- ClassQuiz3/src/Q3.java
+++ ClassQuiz3/src/Q3.java
@@ -1,2 +1,11 @@
 public class Q3 {
+    public static void main(String[] args) {
+
+        short numPets = 5;
+        int numGrains = 5.6;
+        String name = "Scruffy";
+        numPets.length();
+        numGrains.length();
+        name.length();
+    }
 }
