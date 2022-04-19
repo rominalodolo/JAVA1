d34bde52888aa34b7cc8737c47fa0434e294b745
rominalodolo <lodoloromina@gmail.com>
2022-04-19 13:25:58 +0200

    update

--- ClassQuiz3/src/Q4.java
+++ ClassQuiz3/src/Q4.java
@@ -1,2 +1,13 @@
-public class Q4 {
+package aquarium;
+public class Q4 { }
+
+Package aquarium.jellies;
+public clas Jelly { }
+
+        package visitor;
+        import aquarium.*;
+        import aquarium.jellies.Jelly.*;
+
+public class AquariumVisitor {
+    public void admire (Jelly Jelly){}
 }
