538d71cadcb7508b8bb4bc606e235373bf328d21
rominalodolo <lodoloromina@gmail.com>
2022-04-19 18:56:28 +0200

    u

--- ClassQuiz3/src/my/city/School.java
+++ ClassQuiz3/src/my/city/School.java
@@ -1,5 +1,14 @@
-package my.city;
-
-public class School {
-
-}
+//package my.city;
+//
+//import my.school.*;
+//
+//public class School {
+//    public static void main(String[] args) {
+//        System.out.println(Classroom.globalKey);
+//        Classroom room = new Classroom(101, "" Mrs Anderson");
+//        System.out.println(Classroom.roomNumber);
+//        System.out.println(room.floor);
+//        System.out.println(room.teacherName);
+//    }
+//
+//}
