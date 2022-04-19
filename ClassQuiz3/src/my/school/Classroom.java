d72a646071e2d11c6b42e38cdc7a179c2a216566
rominalodolo <lodoloromina@gmail.com>
2022-04-19 14:30:06 +0200

    new

---
+++ ClassQuiz3/src/my/school/Classroom.java
@@ -0,0 +1,13 @@
+package my.school;
+
+public class Classroom {
+    private int roomNumber;
+    protected String teacherName;
+    static int globalKey = 54321;
+    public int floor = 3;
+    Classroom(int r, Sting t){
+        roomNumber = r;
+        teacherName = t;
+    }
+}
+
