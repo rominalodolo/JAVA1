ef9fbf32657f2dbe98e043c480b2604e41bbd15d
rominalodolo <lodoloromina@gmail.com>
2022-04-19 13:59:57 +0200

    update

--- ClassQuiz3/src/Fish.java
+++ ClassQuiz3/src/Fish.java
@@ -1,2 +1,9 @@
 public class Fish {
+    public static void main(String[] args) {
+        int numFish = 4;
+        String fishType = "tuna";
+        String anotherFish = numFish + 1;
+        System.out.println(anotherFish + " " + fishType);
+        System.out.println(numFish + " " + 1);
+    }
 }
