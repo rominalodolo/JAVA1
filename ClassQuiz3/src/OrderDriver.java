7b1a1d82d2b657b1b3e92e446e553e0cc457bba3
rominalodolo <lodoloromina@gmail.com>
2022-04-19 18:56:36 +0200

    u

---
+++ ClassQuiz3/src/OrderDriver.java
@@ -0,0 +1,11 @@
+// Question 23 cont.
+
+public class OrderDriver {
+    public static void main(String[] args) {
+        System.out.println(Order.result + " ");
+        System.out.println(Order.result + " ");
+        new Order();
+        new Order();
+        System.out.println(Order.result + " ");
+    }
+}
