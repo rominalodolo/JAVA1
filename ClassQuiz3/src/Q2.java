278f4d19946eb7c5ffed7a89603c12ee63207c13
rominalodolo <lodoloromina@gmail.com>
2022-04-19 11:56:16 +0200

    update

--- ClassQuiz3/src/Q2.java
+++ ClassQuiz3/src/Q2.java
@@ -1,2 +1,10 @@
 public class Q2 {
+    private String brand;
+    private boolean empty;
+
+    public static void main(String[] args) {
+        Q2 q2 = new Q2();
+        System.out.println("Empty = " + q2.empty);
+        System.out.println(", Brand = " + q2.brand);
+    }
 }
