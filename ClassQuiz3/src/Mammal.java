14a14a18cee2e2cc3a8238166bf1b4ea4132a3fc
rominalodolo <lodoloromina@gmail.com>
2022-04-19 19:02:53 +0200

    update

--- ClassQuiz3/src/Mammal.java
+++ ClassQuiz3/src/Mammal.java
@@ -6,3 +6,12 @@ class Mammal {
     }
 }
 
+public class Platypus extends Mammal {
+    public Platypus() {
+        System.out.println("Platypus");
+    }
+}
+
+    public static void main(String[] args) {
+        new Mammal(5)
+    }
---
+++ ClassQuiz3/src/Q27/HasExoskeleton.java
@@ -0,0 +1,4 @@
+package Q27;
+
+public interface HasExoskeleton {
+}
