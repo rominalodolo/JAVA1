1df98069683de88a0e7fbe03317a91f7652ebebc
Romina <lodoloromina@gmail.com>
2022-07-01 12:54:33 +0200

    update

--- DummiesJavaFx/RolePlayerApp/src/main/java/com/example/roleplayerapp/RolePlayerApplication.java
+++ DummiesJavaFx/RolePlayerApp/src/main/java/com/example/roleplayerapp/RolePlayerApplication.java
@@ -1,23 +1,82 @@
 package com.example.roleplayerapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
 
 public class RolePlayerApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RolePlayerApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static void main(String[] args) {
-        launch();
+
+        launch(args);
     }
+
+
+    TextField txtCharacter;
+    TextField txtActor;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Label lblCharacter = new Label("Character's Name:");
+        lblCharacter.setMinWidth(100);
+        lblCharacter.setAlignment(Pos.BOTTOM_RIGHT);
+
+        txtCharacter = new TextField();
+        txtCharacter.setMinWidth(200);
+        txtCharacter.setMaxWidth(200);
+        txtCharacter.setPromptText(
+                "Enter the name of the character here.");
+
+        
+
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Role Player");
+        primaryStage.show();
+    }
+
+    public void btnOK_Click()
+    {
+        String errorMessage = "";
+
+        if (txtCharacter.getText().length() == 0)
+        {
+            errorMessage += "\nCharacter is a required field.";
+        }
+
+        if (txtActor.getText().length() == 0)
+        {
+            errorMessage += "\nActor is a required field.";
+        }
+
+        if (errorMessage.length() == 0)
+        {
+            String message = "The role of "
+                    + txtCharacter.getText()
+                    + " will be played by "
+                    + txtActor.getText()
+                    + ".";
+            Alert a = new Alert(Alert.AlertType.INFORMATION, message);
+            a.setTitle("Cast");
+            a.showAndWait();
+        }
+        else
+        {
+            Alert a = new Alert(Alert.AlertType.WARNING, errorMessage);
+            a.setTitle("Missing Data");
+            a.showAndWait();
+        }
+    }
+
+
 }
